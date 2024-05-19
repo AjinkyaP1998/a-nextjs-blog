bde6ecb2177b919bc3e8905c83ffdc4f291b98dd
ajinkya_p <ajinkya@optimoloan.com>
2024-05-19 17:15:35 +0530

    release ready for web

--- src/app/my_posts/post_1/post_content.tsx
+++ src/app/my_posts/post_1/post_content.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HerzlImage from './assets/theodore_herzl.png';
-import MapImage from './assets/map.png';
+import MapImage_1 from './assets/map_1.png';
+import MapImage_2 from './assets/map_2.png';
 
 
 function PostContent() {
@@ -12,82 +13,80 @@ function PostContent() {
             <div className = "actual-content">
                 
                 <p className="mb-4">
-                    In 1891, an Austrian journalist named Theodore Herzl arrived in Paris for his new assignment.
-                    He was a Jew, and Paris was the land of the Revolution where he expected equality and free thought.
-                    Yet anti-Semitism was pervasive in France, and a famous military scandal, the Dreyfus affair,
-                    where a French Captain (a Jew) was falsely accused of selling military secrets to Germany, reaffirmed this.
+                The Jews are originally known as Israelites, and they originated around 1,000 BC in West Asia, roughly the same place as modern-day Israel. Jews trace their ancestry to three people: Abraham, his son Isaac, and his grandson Jacob. They believe that God promised Abraham a homeland in present-day Israel. However, due to multiple invasions by the Assyrian Empire, the Babylonians, the Persian Empire, the Roman Empire, and finally the Ottomans, Jews left their homeland.
                 </p>
-                <div className="flex flex-col md:flex-row items-center md:items-start">
-                    <div className="md:flex-1 md:order-2">
+
+                <p className="mb-4">
+                In 1891, an Austrian journalist named Theodore Herzl arrived in Paris for his new assignment. He was a Jew, and Paris was the land of the Revolution where he expected equality and free thought. Yet anti-Semitism was pervasive in France, and a famous military scandal, the Dreyfus affair, where a French Captain (a Jew) was falsely accused of selling military secrets to Germany, reaffirmed this.
+                </p>
+
+                <p className="mb-4">
+                This experience made Herzl realize his life's mission: to find a country for Jews, a place they could call home, a place where they could live without discrimination. Herzl decided on Palestine, the ancient homeland of Jews, and founded a new movement for Jewish statehood, often called Zionism. Herzl is known as the founder of political Zionism, but he died before Israel was created.
+                </p>
+
+                <div className="flex">
+                    <div className="md:w-[20%] md:order-1">
                         <img 
                             src={HerzlImage.src} 
                             alt="Theodore Herzl" 
-                            className="max-w-xs mx-auto md:max-w-sm" 
+                            className="" 
                             style={{ border: '3px solid #424874' }}
                         />
-                        <h2 className="text-xl font-semibold text-center" style={{ marginTop: '0.25rem' }}>Theodore Herzl</h2>
+                        <h2 className="text-base font-semibold text-center" style={{ marginTop: '0.25rem' }}>Theodore Herzl</h2>
                     </div>
-                    <div className="md:flex-1 md:order-1 md:mr-6">
+                    <div className="md:flex-1 md:order-2 ml-6">
                         <p className="mb-4">
-                            This experience made Herzl realize his life&apos;s mission: to find a country for Jews, a place they could call home,
-                            a place where they could live without discrimination. Herzl decided on Palestine,
-                            the ancient homeland of Jews, and founded a new movement for Jewish statehood, often called Zionism.
-                            Herzl is known as the founder of political Zionism, but he died before Israel was created.
+                        By the 19th and 20th centuries, Jews were spread across countries and continents, with large diasporas in Poland, Russia, Britain, and Germany. As anti-Semitism increased, Zionism emerged, led by the likes of Theodore Herzl. He organized a World Congress of Zionists in Basel, Switzerland, where the plan for creating a Jewish homeland in Palestine was made.
                         </p>
                         <p className="mb-4">
-                            The Jews are originally known as Israelites, and they originated around 1,000 BC in West Asia,
-                            roughly the same place as modern-day Israel. Jews trace their ancestry to three people: Abraham,
-                            his son Isaac, and his grandson Jacob. They believe that God promised Abraham a homeland in present-day Israel.
-                            However, due to multiple invasions by the Assyrian Empire, the Babylonians, the Persian Empire, the Roman Empire,
-                            and finally the Ottomans, Jews left their homeland.
+                        However, Palestine was home to tens of thousands of Arab people. Herzl and his company made an offer to the Ottoman Sultan to pay off their foreign debt in return for letting them colonize Palestine, but the Sultan rejected it. Instead, Jews started buying land from Ottoman landlords in Palestine, often funded by wealthy Jews from America and Europe.
+                        </p>
+                        <p className="mb-4">
+                        The British, who were the superpower at the time, were initially lukewarm to the Zionist cause. However, during the World War, they needed the support of Jews and offered complete support for a homeland in Palestine. After the war, Britain was given control of Palestine, and Jewish migration increased.
                         </p>
                     </div>
                 </div>
-                <p className="mb-4">
-                    By the 19th and 20th centuries, Jews were spread across countries and continents, with large diasporas in
-                    Poland, Russia, Britain, and Germany. As anti-Semitism increased, Zionism emerged, led by the likes of Theodore Herzl.
-                    He organized a World Congress of Zionists in Basel, Switzerland, where the plan for creating a Jewish homeland in Palestine was made.
-                </p>
-                <p className="mb-4">
-                    However, Palestine was home to tens of thousands of Arab people.
-                    Herzl and his company made an offer to the Ottoman Sultan to pay off their foreign debt in return for letting them colonize Palestine,
-                    but the Sultan rejected it. Instead, Jews started buying land from Ottoman landlords in Palestine,
-                    often funded by wealthy Jews from America and Europe.
-                </p>
                 <p className="mb-4">
                     The British, who were the superpower at the time, were initially lukewarm to the Zionist cause.
                     However, during the World War, they needed the support of Jews and offered complete support for a homeland in Palestine.
                     After the war, Britain was given control of Palestine, and Jewish migration increased.
                 </p>
-                <p className="mb-4">
-                    As the Jewish population in Palestine increased, tensions with the Arab population escalated.
-                    The British did what they do best: they suggested partitioning a Jewish State for the Jews and an Arab State for the Palestinians.
-                    However, the Arab side rejected this. After the second world war, Britain left the issue to the United Nations, which proposed a plan where the Jewish state would get 55% of Palestine and the Arab state would get 44%, with the remaining 1% given to Jerusalem to be controlled by an international regime.
-                </p>
-                <p className="mb-4">
-                    Palestinians rejected the plan, but Jews declared the state of Israel in 1948. Arab countries attacked
-                    Israel immediately, but Israel won the war and occupied 77% of Palestine. In 1967, another war broke out,
-                    and Israel captured the West Bank, Gaza, and the Sinai Peninsula.
-                </p>
-                <div className="flex flex-col md:flex-row items-center justify-center md:items-center md:justify-start" style={{ height: '100%' }}>
-                    <div className="md:flex-1 md:order-1"  style={{ flex: '0 0 60%' }} >
+
+                <div className="flex">
+                    <div className="md:flex-1 md:order-2">
+                        <p className="mb-4">
+                            As the Jewish population in Palestine increased, tensions with the Arab population escalated. British did what they do best they said let's partition a Jewish State for the Jews and an Arab State for the Palestinians. However, the Arab side rejected this. After the second world war, Britain left the issue to the United Nations, which proposed a plan where the Jewish state would get 55% of Palestine and the Arab state would get 44%, with the remaining 1% given to Jerusalem to be controlled by an international regime.
+                        </p>
+                        <p className="mb-4">
+                            Palestinians rejected the plan, but Jews declared the state of Israel in 1948. Arab countries attacked Israel immediately, but Israel won the war and occupied 77% of Palestine. In 1967, another war broke out, and Israel captured the West Bank, Gaza, and the Sinai Peninsula.
+                        </p>
+                    </div>  
+                    <div className="md:w-[30%] md:order-2">
                         <img 
-                            src={MapImage.src} 
-                            alt="MapImage" 
+                            src={MapImage_1.src} 
+                            alt="MapImage_1" 
                             className="mx-auto" 
-                            style={{ width: '90%', border: '3px solid #424874' }}
+                            style={{ border: '3px solid #424874' }}
                         />
                     </div>
-                    <div className="md:flex-1 md:order-2 md:mr-6"   style={{ flex: '0 0 40%' }} >
+                </div>
+
+                <div className="flex">
+                    <div className="md:w-[40%] md:order-1">
+                        <img 
+                            src={MapImage_2.src} 
+                            alt="MapImage_2" 
+                            className="" 
+                            style={{ border: '3px solid #424874' }}
+                        />
+                    </div>
+                    <div className="md:flex-1 md:order-2 md:mr-6 ml-6">
                         <p className="mb-4">
-                            Today, Palestine wants statehood based on the pre-1967 borders, meaning the whole of West Bank, Gaza, and East Jerusalem.
-                            Israel, however, maintains that this is not possible as they have built dozens of settlements inside the West Bank.
-                            The resolution looks far away. This conflict has been named a regional conflict as both sides claim the same land,
-                            but it is also an exported one. Historically, Europe discriminated against Jews, not Palestinians or Ottomans;
-                            they were compelled to relocate. Later, during the war, they required Jewish support and thus promised them a homeland.
-                            Yet, post-war difficulties prompted them to execute a classic colonial act, a tactic they often employ.
-                            The question of Palestine cannot be resolved through weaponry but demands political solutions. Hope rests in moderates from
-                            both factions charting a peaceful path towards coexistence.
+                            Today, Palestine wants statehood based on the pre-1967 borders, meaning the whole of West Bank, Gaza, and East Jerusalem. Israel, however, maintains that this is not possible as they have built dozens of settlements inside the West Bank. The resolution looks far away. This conflict has been named a regional conflict as both sides claim the same land, but it is also an exported one. Historically, Europe discriminated against Jews, not Palestinians or Ottomans; they were compelled to relocate. Later, during the war, they required Jewish support and thus promised them a homeland. Yet, post-war difficulties prompted them to execute a classic colonial act, a tactic they often employ.
+                        </p>
+
+                        <p className="mb-4">
+                            The question of Palestine cannot be resolved through weaponry but demands political solutions. Hope rests in moderates from both factions charting a peaceful path towards coexistence.
                         </p>
                     </div>
                 </div>
--- src/components/intro_page.tsx
+++ src/components/intro_page.tsx
@@ -1,58 +1,32 @@
 'use client'
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Image from 'next/image';
 
 function IntroPage() {
-    const [opacity, setOpacity] = useState(1);  // Starts fully visible
-
-    // Toggle opacity
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setOpacity(prev => (prev === 1 ? 0 : 1));  // Toggle between 0 and 1
-        }, 6000); // Change opacity every 6 seconds to allow 1 second for full visibility
-
-        return () => clearInterval(intervalId);
-    }, []);
-
     return (
         <div className="intro-page">
             <div className="relative w-full h-full mx-auto flex">
-                <div className="w-1/2 flex items-center justify-center h-full">
-                    <div>
+                <div className="relative w-1/2 h-full flex items-center justify-center">
+                    <div className="text-center" >
                         <h1 className="intro-page-text">
                             Journey through the History of Global Politics. Uncover key events, alliances, and conflicts that have shaped our world. 
                             See how past decisions echo in today&apos;s international landscape...
                         </h1>
                     </div>
                 </div>
-                <div className="w-1/2 flex justify-center items-center">
+                <div className="relative w-1/2 h-full flex items-center justify-center">
                     <div className="intro-image-container">
                         <Image
                             src="/intro_image.webp"
-                            alt="Dark Image"
+                            alt="Introduction Image"
                             layout="fill"
                             objectFit="cover"
                             priority
-                            className="intro-image-fade"
-                            style={{ opacity: opacity }}
-                        />
-                        <Image
-                            src="/intro_image.webp"
-                            alt="Bright Image"
-                            layout="fill"
-                            objectFit="cover"
-                            priority
-                            className="intro-image-fade"
-                            style={{ opacity: 1 - opacity }}
                         />
                     </div>
                 </div>
             </div>
-
-            {/* <div className="intro-text absolute top-1/3 left-0 right-0 text-center text-black text-6xl font-bold tracking-wide">
-                <h1 className="font-serif" style={{ fontFamily: '"Times New Roman", serif' }}>Welcome to the GeoBlog!<br />Explore the world through geopolitics.</h1>
-            </div> */}
         </div>
     );
 }
